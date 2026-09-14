9bc07d84f42b6a9c9fb8a136c6a671c101c32c9a
Miquel <miquel.vilimelis@gmail.com>
2026-09-14 12:58:02 +0200

    feat: añade modelo Publicidad y Web

---
+++ src/app/features/service-selector/models/publicidad.model.spec.ts
@@ -0,0 +1,7 @@
+import { Publicidad } from './publicidadmodel';
+
+describe('Publicidad', () => {
+  it('should create an instance', () => {
+    expect(new Publicidad()).toBeTruthy();
+  });
+});
---
+++ src/app/features/service-selector/models/web.model.spec.ts
@@ -0,0 +1,7 @@
+import { Web } from './webmodel';
+
+describe('Web', () => {
+  it('should create an instance', () => {
+    expect(new Web()).toBeTruthy();
+  });
+});
