92542a3e709c17f3058ccd88344b4ec8caca8e98
Miquel <miquel.vilimelis@gmail.com>
2026-09-16 22:45:23 +0200

    feat: Conecta RepositorioPresupuestosLocalStorage como proveedor de RepositorioPresupuestos
    Sin este paso, Angular no sabe qué instancia concreta entregar al inyectar la clase abstracta.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { RepositorioPresupuestos } from './data/repositorio-presupuestos.model';
+import { RepositorioPresupuestosLocalStorage } from './data/local-storage/repositorio-presupuestos-local-storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    { provide: RepositorioPresupuestos, useClass: RepositorioPresupuestosLocalStorage }
   ]
 };
